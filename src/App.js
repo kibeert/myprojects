cf40b07d49c242c72e81b66c363bb6adb5ca4610
kibeert <ckibertkoech@gmail.com>
2025-01-14 08:37:41 +0300

    `feat: updated public/index.html and src/App.js with new content`

--- src/App.js
+++ src/App.js
@@ -4,9 +4,31 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Hello World</h1>
+      <table>
+        <tr>
+          <th>Name</th>
+        </tr>
+        <tr>
+          <td>John</td>
+        </tr>
+        <tr>
+          <td>Elsa</td>
+        </tr>
+      </table>
+      <h1>React is {5+5} times better with JSX</h1>
+      <h1>{myElent}</h1>
+      
     </div>
+    
   );
 }
+const x = 7;
+let text = "Goodbye";
+if (x<10){
+  text = "Hello";
+}
+const myElent = <h1>{text}</h1>;
 
 export default App;
+
